138c63c82697c4d2f158f954f57920753336e93a
Esteban Indiveri <esteban.indiveri@gmail.com>
2021-11-22 17:55:28 -0300

    mocked and test querys

---
+++ index.js
@@ -0,0 +1,38 @@
+const { ApolloServer, gql } = require("apollo-server");
+
+const typeDefs = gql`
+  type Curso {
+    titulo: String
+    tecnologia: String
+  }
+  type Query {
+    obtenerCursos: [Curso]
+  }
+`;
+
+const cursos = [
+  {
+    titulo: "Javascript 2021",
+    tecnologia: "Javascript ES6",
+  },
+  {
+    titulo: "React la guia de 2021",
+    tecnologia: "React JS",
+  },
+  {
+    titulo: "Flutter el SDK de google",
+    tecnologia: "Flutter",
+  },
+];
+
+const resolvers = {
+  Query: {
+    obtenerCursos: () => cursos,
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.listen().then(({ url }) => {
+  console.log(`Servidor listo en la URL ${url}`);
+});
